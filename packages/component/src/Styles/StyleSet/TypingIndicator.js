01170f776faf807c05e220be61cb93e671b30215
Ping Yip <ping.yip@hotmail.com>
2020-03-28 01:14:42 +0800

    TypingAnimator.js

--- packages/component/src/Styles/StyleSet/TypingIndicator.js
+++ packages/component/src/Styles/StyleSet/TypingIndicator.js
@@ -2,11 +2,11 @@ export default function createTypingIndicatorStyle({ paddingRegular }) {
   return {
     paddingBottom: paddingRegular,
 
-    '&:not(.webchat__typingIndicator--rtl)': {
+    '&:not(.webchat__typing-indicator--rtl)': {
       paddingLeft: paddingRegular
     },
 
-    '&.webchat__typingIndicator--rtl': {
+    '&.webchat__typing-indicator--rtl': {
       paddingRight: paddingRegular
     }
   };
