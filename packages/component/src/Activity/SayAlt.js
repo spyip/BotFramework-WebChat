6d9553466fed5196285433eca3c65eff9876bfe9
Ping Yip <ping.yip@hotmail.com>
2020-03-31 18:08:03 +0800

    SayAlt.js

--- packages/component/src/Activity/SayAlt.js
+++ packages/component/src/Activity/SayAlt.js
@@ -1,14 +1,17 @@
 import { css } from 'glamor';
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 // TODO: [P3] Although this is for development purpose, prettify it
 const ROOT_CSS = css({
-  color: 'Red',
-  margin: 0
+  '&.webchat__say-alt': {
+    color: 'Red',
+    margin: 0
+  }
 });
 
-const SayAlt = ({ speak }) => !!speak && <pre className={ROOT_CSS}>{speak}</pre>;
+const SayAlt = ({ speak }) => !!speak && <pre className={classNames(ROOT_CSS + '', 'webchat__say-alt')}>{speak}</pre>;
 
 SayAlt.defaultProps = {
   speak: ''
